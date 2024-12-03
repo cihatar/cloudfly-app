d33d9e1e0e85b6c3f212e46aa99f3b243d27da58
cihatar <cihatarman2003@gmail.com>
2024-12-03 14:21:51 +0300

    Add axios comment

--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -5,4 +5,18 @@ const customAxios = axios.create({
     withCredentials: true,
 });
 
+/*
+// check if the response status is 401, and redirect to home page
+customAxios.interceptors.response.use(
+    (response) => response, 
+    (error) => {
+      if (error.response && error.response.status === 401) {
+        localStorage.removeItem('user');
+        window.location.href = '/auth/login';
+      }
+      return Promise.reject(error);
+    }
+);
+*/
+
 export default customAxios;
